49e1c438778fa31d44abee9a01511aaa85eca427
apollyon0728 <93040673@qq.com>
2019-01-16 16:31:56 +0800

    [feature] KNN 近邻算法 《机器学习实战》

--- knn/kNN.py
+++ knn/kNN.py
@@ -1,8 +1,152 @@
+# coding=utf-8
+'''
+# 《机器学习实战》第二章  (使用Py2运行） p23
+
+将每组数据划分到某个类别中：
+1、计算已知类别数据集中的点，与当前点之间的距离
+2、按照距离递增次序排序
+3、选取与当前点距离最小的k个点
+4、确定前K个点所在类别的出现频率
+5、返回前K个点出现频率最高的类别作为当前点的预测分类
+
+Created on Sep 16, 2010
+kNN: k Nearest Neighbors
+
+Input:      inX: vector to compare to existing dataset (1xN)
+            dataSet: size m data set of known vectors (NxM)
+            labels: data set labels (1xM vector)
+            k: number of neighbors to use for comparison (should be an odd number)
+            
+Output:     the most popular class label
+
+@author: pbharrin
+'''
+
 from numpy import *
 import operator
+from os import listdir
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    # 距离计算 begin
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    # 距离计算 end
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  # 选取距离最小的k个点
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)  # 排序
+    return sortedClassCount[0][0]
 
 
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
+
+
+def file2matrix(filename):
+    love_dictionary = {'largeDoses': 3, 'smallDoses': 2, 'didntLike': 1}
+    fr = open(filename)
+    arrayOLines = fr.readlines()    # 得到文件行数
+    numberOfLines = len(arrayOLines)  # get the number of lines in the file
+    returnMat = zeros((numberOfLines, 3))  # prepare matrix to return （返回创建的NumPy矩阵）
+    classLabelVector = []  # prepare labels return
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]  # 解析文件数据到列表
+        if listFromLine[-1].isdigit():
+            classLabelVector.append(int(listFromLine[-1]))
+        else:
+            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+# 数值归一化
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)    # 参数0使得函数可以从列中选取最小值，而不是选取当前行的最小值
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))  # element wise divide （特征值相除）
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.50  # hold out 10%
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')  # load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount / float(numTestVecs))
+    print errorCount
+
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(raw_input( \
+        "percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream, ])
+    classifierResult = classify0((inArr - \
+                                  minVals) / ranges, normMat, datingLabels, 3)
+    print "You will probably like this person: %s" % resultList[classifierResult - 1]
+
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+trainingDigits = ""
+
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')  # load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')  # iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print
+        "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print
+    "\nthe total number of errors is: %d" % errorCount
+    print
+    "\nthe total error rate is: %f" % (errorCount / float(mTest))
---
+++ knn/kNNTest-11.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+'''
+    kNN 的测试类
+    kNN.datingClassTest() # 输出预测结果、错误率
+'''
+
+import sys
+import kNN
+
+sys.path.append(r"E:/workSpaces/workSpaces-Git-Python/tensorflow_practice/knn")
+
+# kNN.datingClassTest()  # 输出预测结果、错误率
+kNN.classifyPerson()
